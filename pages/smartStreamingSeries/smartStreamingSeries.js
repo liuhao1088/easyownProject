7a12d76e0161cf425df0afd0ffbb47637f3dfaef
liuhao1088 <1350569467@qq.com>
2020-10-12 15:19:59 +0800

    更新

--- pages/smartStreamingSeries/smartStreamingSeries.js
+++ pages/smartStreamingSeries/smartStreamingSeries.js
@@ -1,37 +1,38 @@
-// pages/wode/wode.js
+// pages/smartStreamingSeries/smartStreamingSeries.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgUrl:[
-      'https://img11.360buyimg.com/ddimg/jfs/t1/141106/6/10567/458756/5f83f33bEe17ec49e/0f7c2eb5d48f2da1.png',
-      'https://img11.360buyimg.com/ddimg/jfs/t1/134409/33/12264/573162/5f83f3f3Eba2e86e1/1db56261b23a5887.png',
-      'https://img11.360buyimg.com/ddimg/jfs/t1/155527/31/2019/911490/5f83f40cE99a615d8/b4fb5de2c80c17fe.png'
-    ],
-     active: 0,
-     indicatorDots: true,
-     autoplay: true,
-     interval: 3000,
-     duration: 1000,
-     vertical: true
+
   },
-  hdRecorderSeriesdj:function(e){
+  detailsamElevendj:function(e){
     wx.navigateTo({
-      url: "../hdRecorderSeries/hdRecorderSeries"
+      url: "../detailsamEleven/detailsamEleven"
     })
   },
-  smartStreamingSeriesdj:function(e){
+  detailsamThirtydj:function(e){
     wx.navigateTo({
-      url: "../smartStreamingSeries/smartStreamingSeries"
+      url: "../detailsamThirty/detailsamThirty"
     })
   },
+  detailsamSixtysixdj:function(e){
+    wx.navigateTo({
+      url: "../detailsamSixtysix/detailsamSixtysix"
+    })
+  },
+  detailsamEightyeightdj:function(e){
+    wx.navigateTo({
+      url: "../detailsamEightyeight/detailsamEightyeight"
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
@@ -45,13 +46,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    wx.showShareMenu({
 
-      withShareTicket:true,
-      
-      menus:['shareAppMessage','shareTimeline']
-      
-      })
   },
 
   /**
